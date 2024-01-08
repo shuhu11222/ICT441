67611e185a3e674c12c7ca7d4a0e5c31120965a8
Nanshenger <2584036741@qq.com>
2024-01-08 16:06:04 +0800

    'upadte'

---
+++ assets/js/Home.js
@@ -0,0 +1,11 @@
+function readmore(){ 
+    var input=prompt("Please choose your content 'student'or'parent'")
+        if (input == "student"){
+         window.location.href="Enquiry.html" 
+        }
+        
+        else if (input == "parent")
+        {
+            window.location.href="About.html" 
+        }
+    }
